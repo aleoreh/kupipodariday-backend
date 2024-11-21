58d9a440fabb8a1cfa42bef224c93c56a0b331df
Aleksey Orekhov <aleoreh@gmail.com>
2024-11-21 11:44:52 +0300

    Add logging to domain-error-handler.service

--- src/exceptions/domain-error-handler.service.ts
+++ src/exceptions/domain-error-handler.service.ts
@@ -1,18 +1,30 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { AccessDeniedError } from './access-denied.error';
-import { DomainError } from './domain-error';
 import { AlreadyExistsError } from './already-exists.error';
+import { DomainError } from './domain-error';
 
 @Injectable()
 export class DomainErrorHandler {
+  private readonly logger = new Logger(DomainErrorHandler.name);
+
   toHttp(error: DomainError): HttpException {
-    let httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    let httpStatus: HttpStatus | null = null;
 
     if (error instanceof AccessDeniedError) httpStatus = HttpStatus.FORBIDDEN;
 
     if (error instanceof AlreadyExistsError)
       httpStatus = HttpStatus.BAD_REQUEST;
 
-    throw new HttpException(error.message, httpStatus);
+    Logger.error(error.message, error.stack);
+
+    switch (httpStatus) {
+      case null:
+        throw new HttpException(
+          'Ошибка на стороне сервере',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      default:
+        throw new HttpException(error.message, httpStatus);
+    }
   }
 }
