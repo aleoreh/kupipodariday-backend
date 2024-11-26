4275182d1c293f772b6939483456677cc2be6f68
Aleksey Orekhov <aleoreh@gmail.com>
2024-11-26 10:27:37 +0300

    Implement adding offer logic

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { UpdateOfferDto } from './dto/update-offer.dto';
 import { Offer } from './entities/offer.entity';
 import { Wish } from '../wishes/entities/wish.entity';
+import { BadOfferError } from '../errors/bad-offer.error';
 
 @Injectable()
 export class OffersService {
@@ -16,22 +17,46 @@ export class OffersService {
     private userRepository: Repository<User>,
     @InjectRepository(Wish)
     private wishRepository: Repository<Wish>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async create(createOfferDto: CreateOfferDto, userId: number) {
-    const res = await Promise.all([
+    const [user, wish] = await Promise.all([
       this.userRepository.findOneBy({ id: userId }),
-      this.wishRepository.findOneBy({ id: createOfferDto.itemId }),
-    ]).then(([user, wish]) => {
-      const offer = this.offerRepository.create({
-        ...createOfferDto,
-        item: wish,
-        user,
-      });
-      return this.offerRepository.save(offer);
+      this.wishRepository.findOne({
+        where: { id: createOfferDto.itemId },
+        relations: { owner: true },
+      }),
+    ]);
+
+    if (userId === wish.owner.id)
+      throw new BadOfferError('Нельзя вносить деньги на собственные желания');
+
+    const newRaised = wish.raised + createOfferDto.amount;
+
+    if (newRaised > wish.price)
+      throw new BadOfferError('Собранная сумма превышает стоимость желания');
+
+    const offer = this.offerRepository.create({
+      ...createOfferDto,
+      item: wish,
+      user,
     });
 
-    return res;
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await this.wishRepository.save({ ...wish, raised: newRaised });
+      const res = await this.offerRepository.save(offer);
+      await queryRunner.commitTransaction();
+      return res;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async findAll() {
