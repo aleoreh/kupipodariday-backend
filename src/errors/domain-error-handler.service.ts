0c7c9e97c76ce903695fc7e8cbd564cc83f945c5
Aleksey Orekhov <aleoreh@gmail.com>
2024-11-26 10:26:22 +0300

    Add bad-offer.error

--- src/errors/domain-error-handler.service.ts
+++ src/errors/domain-error-handler.service.ts
@@ -3,6 +3,7 @@ import { AccessDeniedError } from './access-denied.error';
 import { AlreadyExistsError } from './already-exists.error';
 import { DomainError } from './domain-error';
 import { UserNotFoundError } from './user-not-found.error';
+import { BadOfferError } from './bad-offer.error';
 
 @Injectable()
 export class DomainErrorHandler {
@@ -15,6 +16,8 @@ export class DomainErrorHandler {
       httpStatus = HttpStatus.BAD_REQUEST;
 
     if (error instanceof UserNotFoundError) httpStatus = HttpStatus.NOT_FOUND;
+
+    if (error instanceof BadOfferError) httpStatus = HttpStatus.BAD_REQUEST;
 
     // используем статический метод логгера, так как метод toHttp используется
     // в контроллерах без привязки this (promise.catch(this.exceptionHandler.toHttp))
